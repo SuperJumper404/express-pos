d5bef3a267b076041bfeda721a15df1447facab2
joker <ns.ayness@gmail.com>
2025-07-01 19:03:07 +0200

    deploiment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,10 +9,6 @@ module.exports = {
       max_memory_restart: "2G",
       env: {
         NODE_ENV: "staging",
-        PORT: 5000, // ou ton port réel
-        DB_HOST: "localhost", // exemple, à adapter selon ton app
-        DB_USER: "user",
-        DB_PASS: "password",
       },
     },
   ],
